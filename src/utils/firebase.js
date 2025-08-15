9473b2732e519a7030ff5cebf65a0e58f4ab442f
Aksh Khurana <khuranaaksh08@gmail.com>
2025-08-15 23:32:57 +0530

    updated firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBFjx-djJanb3hduExoq0lFFBYJ2l1B-Ic",
-  authDomain: "netflixgpt-7511b.firebaseapp.com",
-  projectId: "netflixgpt-7511b",
-  storageBucket: "netflixgpt-7511b.firebasestorage.app",
-  messagingSenderId: "406153568685",
-  appId: "1:406153568685:web:612a0c9f1bc0c6c0094124",
-  measurementId: "G-3XNB77YLGE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
